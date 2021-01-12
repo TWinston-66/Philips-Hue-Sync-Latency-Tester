f4503d62991df66bb74ebb9cb9cff1690ff7dca7
TWinston-66 <65579453+TWinston-66@users.noreply.github.com>
2021-01-11 19:09:07 -0700

    Update Hue_Sync.py

--- Hue_Sync.py
+++ Hue_Sync.py
@@ -6,6 +6,6 @@ turtle.title("COLORS")
 colors = ["green", "red", "blue", "orange"]
 
 while(True):
-    for x in colors: 
+    for color in colors: 
         time.sleep(0.5)
         turtle.bgcolor(x)
